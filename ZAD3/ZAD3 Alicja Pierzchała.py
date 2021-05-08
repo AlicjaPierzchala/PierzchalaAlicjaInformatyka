ecaf78476d1190a8b7ea99301392caee7b034573
AlicjaPierzchala <pa00956@zut.edu.pl>
2021-05-08 18:55:27 +0200

    ZAD3 średnia

---
+++ ZAD3/ZAD3 Alicja Pierzchała.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  8 12:19:14 2021
+
+@author: rawsk
+"""
+
+def Nazwafunkcji():
+    print("wynik funkcji")
+    print("końcowy wynik")
+    
+def Funkcjaargumentów (zmienna):
+    print("podałam zmienną która wynosi = ", zmienna)
+    
+a = 8
+b = 1
+c = 3
+
+for i in range (0,3):
+    #if a % 2 == 0:
+    n = a + b +c 
+    i = i + 1
+    print ("wielkosc i = ", i)
+    srednia = n / 3
+  #  else:
+  #  break
+   
+print("suma= ",n )
+print("srednia = ", srednia)    
