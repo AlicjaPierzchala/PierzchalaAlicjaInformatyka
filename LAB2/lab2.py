55449881ef66285743d055208beacd6dc710b054
AlicjaPierzchala <82751308+AlicjaPierzchala@users.noreply.github.com>
2021-05-08 11:50:07 +0200

    LAB2

---
+++ LAB2/lab2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print ('Hello, World')
+       n = 3
+       silnia = 1
+       i = 1
+       while i <= n:
+           
+           silnia = silnia * i
+       print ( 'silnia = ', silnia
+             )
+       i = i + 1
+       print ('silnia =', silnia)
+       i = i + 1
+       print (' i = ' , i)
+       
+       
+       
