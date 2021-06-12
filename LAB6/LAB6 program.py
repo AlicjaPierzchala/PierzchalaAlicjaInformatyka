ff46a34f731bbf97cf35c3a94cfe541ec6bcced1
AlicjaPierzchala <pa00956@zut.edu.pl>
2021-06-12 22:58:01 +0200

    t

---
+++ LAB6/LAB6 program.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 16 10:03:15 2021
+
+@author: rawsk
+"""
+
+
+            
+            
+# x = input("x = ")
+# x = int(x)
+
+# for i in range(-x, x + 1):
+#                       if(i % 2 ==0):
+#                           print(i)
+
+
+def dodawanie (a,b,c):
+x = a + b + c
+return x
+a = 1
+b = 2
+c = 3
+dodawanie(4,5,7)
+
+    print("wynik = ",x )
+    
